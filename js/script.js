36694a417f8993739d943271ca9b0b2b36e7b918
dpinchuk <pinchuk.dap@gmail.com>
2018-09-15 10:06:10 +0300

    bug fixed

--- js/script.js
+++ js/script.js
@@ -16,33 +16,43 @@ let checkNumber = (num) => {
 
 };
 
-let printArr = (arr) => {
-    console.log("Simplex numbers in diapason [1, " + to + "]:");
-    document.write("Simplex numbers in diapason [1, " + to + "]:" + "<br>");
+let printArr = (arr, from, to) => {
+    console.log("Simplex numbers in diapason [" + from + ", " + to + "]:");
+    document.write(`Simplex numbers in diapason [${from}, ${to}]:<br />`);
     arr.forEach((item) => {
-        console.log(item);
-        document.write(item + "<br />");
+        if (item >= from) {
+            console.log(item);
+            document.write(`${item}<br />`);
+        }
     });
 };
 
-let to = prompt("Enter number TO: ");
+let from = Number(prompt("Enter number FROM: "));
+let to = Number(prompt("Enter number TO: "));
 
-while (!checkNumber(to)) {
-    to = prompt("Enter correct number TO: ");
+while (from > to) {
+    from = 0;
+    to = 0;
+    while (!checkNumber(from)) {
+        from = Number(prompt("Enter correct number FROM: "));
+    }
+    while (!checkNumber(to)) {
+        to = Number(prompt("Enter correct number TO: "));
+    }
 }
 
-let getPrimes = (n) => {
-    let a = new Array(parseInt(n = n / 2)),
-        t = (Math.sqrt(4 + 8 * n) - 2) / 4,
+let getPrimes = (to) => {
+    let a = new Array(parseInt(to = to / 2)),
+        t = (Math.sqrt(4 + 8 * to) - 2) / 4,
         u = 0,
         r = [];
-    for (let i = 1; i < (n - 1) / 3; i++) a[1 + 3 * i] = true;
+    for (let i = 1; i < (to - 1) / 3; i++) a[1 + 3 * i] = true;
     for (let i = 2; i <= t; i++) {
-        u = (n - i) / (1 + 2 * i);
+        u = (to - i) / (1 + 2 * i);
         if (i % 3 - 1) for (let j = i; j < u; j++) a[i + j + 2 * i * j] = true;
     }
-    for (let i = 0; i < n; i++) !a[i] && r.push(i * 2 + 1);
+    for (let i = 0; i < to; i++) !a[i] && r.push(i * 2 + 1);
     return r;
 };
 
-printArr(getPrimes(to));
+printArr(getPrimes(to), from, to);
